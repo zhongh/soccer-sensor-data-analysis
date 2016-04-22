6f5c6b11c76933eff7c136edde1adc112e02a022
zhonghao1014 <zhonghao1014@gmail.com>
2016-04-21 22:53:03 -0400

    fixing player labels and fix a little bug in printing player in own half

--- full-game-parser.py
+++ full-game-parser.py
@@ -37,43 +37,43 @@ SID_MAP = {
     "10": {"type": "ball", "label": "ball10"},
     "12": {"type": "ball", "label": "ball12"},
     # Team A:
-    "97": {"type": "glove", "team": "A", "label": "goalkeeperAleftglove", "name": "Nick Gertje", "arm": "left"},
-    "98": {"type": "glove", "team": "A", "label": "goalkeeperArightglove", "name": "Nick Gertje", "arm": "right"},
-    "13": {"type": "player", "team": "A", "label": "GoalkeeperA", "name": "Nick Gertje", "leg": "left"},
-    "14": {"type": "player", "team": "A", "label": "GoalkeeperA", "name": "Nick Gertje", "leg": "right"},
-    "47": {"type": "player", "team": "A", "label": "PlayerA1", "name": "Dennis Dotterweich", "leg": "left"},
-    "16": {"type": "player", "team": "A", "label": "PlayerA1", "name": "Dennis Dotterweich", "leg": "right"},
-    "49": {"type": "player", "team": "A", "label": "PlayerA2", "name": "Niklas Waelzlein", "leg": "left"},
-    "88": {"type": "player", "team": "A", "label": "PlayerA2", "name": "Niklas Waelzlein", "leg": "right"},
-    "19": {"type": "player", "team": "A", "label": "PlayerA3", "name": "Wili Sommer", "leg": "left"},
-    "52": {"type": "player", "team": "A", "label": "PlayerA3", "name": "Wili Sommer", "leg": "right"},
-    "53": {"type": "player", "team": "A", "label": "PlayerA4", "name": "Philipp Harlass", "leg": "left"},
-    "54": {"type": "player", "team": "A", "label": "PlayerA4", "name": "Philipp Harlass", "leg": "right"},
-    "23": {"type": "player", "team": "A", "label": "PlayerA5", "name": "Roman Hartleb", "leg": "left"},
-    "24": {"type": "player", "team": "A", "label": "PlayerA5", "name": "Roman Hartleb", "leg": "right"},
-    "57": {"type": "player", "team": "A", "label": "PlayerA6", "name": "Erik Engelhardt", "leg": "left"},
-    "58": {"type": "player", "team": "A", "label": "PlayerA6", "name": "Erik Engelhardt", "leg": "right"},
-    "59": {"type": "player", "team": "A", "label": "PlayerA7", "name": "Sandro Schneider", "leg": "left"},
-    "28": {"type": "player", "team": "A", "label": "PlayerA7", "name": "Sandro Schneider", "leg": "right"},
+    "97": {"type": "glove", "team": "A", "label": "goalkeeper_A_left_glove", "name": "Nick Gertje", "arm": "left"},
+    "98": {"type": "glove", "team": "A", "label": "goalkeeper_A_right_glove", "name": "Nick Gertje", "arm": "right"},
+    "13": {"type": "player", "team": "A", "label": "goalkeeper_A", "name": "Nick Gertje", "leg": "left"},
+    "14": {"type": "player", "team": "A", "label": "goalkeeper_A", "name": "Nick Gertje", "leg": "right"},
+    "47": {"type": "player", "team": "A", "label": "player_A1", "name": "Dennis Dotterweich", "leg": "left"},
+    "16": {"type": "player", "team": "A", "label": "player_A1", "name": "Dennis Dotterweich", "leg": "right"},
+    "49": {"type": "player", "team": "A", "label": "player_A2", "name": "Niklas Waelzlein", "leg": "left"},
+    "88": {"type": "player", "team": "A", "label": "player_A2", "name": "Niklas Waelzlein", "leg": "right"},
+    "19": {"type": "player", "team": "A", "label": "player_A3", "name": "Wili Sommer", "leg": "left"},
+    "52": {"type": "player", "team": "A", "label": "player_A3", "name": "Wili Sommer", "leg": "right"},
+    "53": {"type": "player", "team": "A", "label": "player_A4", "name": "Philipp Harlass", "leg": "left"},
+    "54": {"type": "player", "team": "A", "label": "player_A4", "name": "Philipp Harlass", "leg": "right"},
+    "23": {"type": "player", "team": "A", "label": "player_A5", "name": "Roman Hartleb", "leg": "left"},
+    "24": {"type": "player", "team": "A", "label": "player_A5", "name": "Roman Hartleb", "leg": "right"},
+    "57": {"type": "player", "team": "A", "label": "player_A6", "name": "Erik Engelhardt", "leg": "left"},
+    "58": {"type": "player", "team": "A", "label": "player_A6", "name": "Erik Engelhardt", "leg": "right"},
+    "59": {"type": "player", "team": "A", "label": "player_A7", "name": "Sandro Schneider", "leg": "left"},
+    "28": {"type": "player", "team": "A", "label": "player_A7", "name": "Sandro Schneider", "leg": "right"},
     # Team B:
-    "99": {"type": "glove", "team": "B", "label": "goalkeeperBleftglove", "name": "Leon Krapf", "arm": "left"},
-    "100": {"type": "glove", "team": "B", "label": "goalkeeperBrightglove", "name": "Leon Krapf", "arm": "right"},
-    "61": {"type": "player", "team": "B", "label": "GoalkeeperB", "name": "Leon Krapf", "leg": "left"},
-    "62": {"type": "player", "team": "B", "label": "GoalkeeperB", "name": "Leon Krapf", "leg": "right"},
-    "63": {"type": "player", "team": "B", "label": "GoalkeeperB", "name": "Kevin Baer", "leg": "left"},
-    "64": {"type": "player", "team": "B", "label": "PlayerB1", "name": "Kevin Baer", "leg": "right"},
-    "65": {"type": "player", "team": "B", "label": "PlayerB2", "name": "Luca Ziegler", "leg": "left"},
-    "66": {"type": "player", "team": "B", "label": "PlayerB2", "name": "Luca Ziegler", "leg": "right"},
-    "67": {"type": "player", "team": "B", "label": "PlayerB3", "name": "Ben Mueller", "leg": "left"},
-    "68": {"type": "player", "team": "B", "label": "PlayerB3", "name": "Ben Mueller", "leg": "right"},
-    "69": {"type": "player", "team": "B", "label": "PlayerB4", "name": "Vale Reitstetter", "leg": "left"},
-    "38": {"type": "player", "team": "B", "label": "PlayerB4", "name": "Vale Reitstetter", "leg": "right"},
-    "71": {"type": "player", "team": "B", "label": "PlayerB5", "name": "Christopher Lee", "leg": "left"},
-    "40": {"type": "player", "team": "B", "label": "PlayerB5", "name": "Christopher Lee", "leg": "right"},
-    "73": {"type": "player", "team": "B", "label": "PlayerB6", "name": "Leon Heinze", "leg": "left"},
-    "74": {"type": "player", "team": "B", "label": "PlayerB6", "name": "Leon Heinze", "leg": "right"},
-    "75": {"type": "player", "team": "B", "label": "PlayerB7", "name": "Leo Langhans", "leg": "left"},
-    "44": {"type": "player", "team": "B", "label": "PlayerB7", "name": "Leo Langhans", "leg": "right"}
+    "99": {"type": "glove", "team": "B", "label": "goalkeeper_B_left_glove", "name": "Leon Krapf", "arm": "left"},
+    "100": {"type": "glove", "team": "B", "label": "goalkeeper_B_right_glove", "name": "Leon Krapf", "arm": "right"},
+    "61": {"type": "player", "team": "B", "label": "goalkeeper_B", "name": "Leon Krapf", "leg": "left"},
+    "62": {"type": "player", "team": "B", "label": "goalkeeper_B", "name": "Leon Krapf", "leg": "right"},
+    "63": {"type": "player", "team": "B", "label": "player_B1", "name": "Kevin Baer", "leg": "left"},
+    "64": {"type": "player", "team": "B", "label": "player_B1", "name": "Kevin Baer", "leg": "right"},
+    "65": {"type": "player", "team": "B", "label": "player_B2", "name": "Luca Ziegler", "leg": "left"},
+    "66": {"type": "player", "team": "B", "label": "player_B2", "name": "Luca Ziegler", "leg": "right"},
+    "67": {"type": "player", "team": "B", "label": "player_B3", "name": "Ben Mueller", "leg": "left"},
+    "68": {"type": "player", "team": "B", "label": "player_B3", "name": "Ben Mueller", "leg": "right"},
+    "69": {"type": "player", "team": "B", "label": "player_B4", "name": "Vale Reitstetter", "leg": "left"},
+    "38": {"type": "player", "team": "B", "label": "player_B4", "name": "Vale Reitstetter", "leg": "right"},
+    "71": {"type": "player", "team": "B", "label": "player_B5", "name": "Christopher Lee", "leg": "left"},
+    "40": {"type": "player", "team": "B", "label": "player_B5", "name": "Christopher Lee", "leg": "right"},
+    "73": {"type": "player", "team": "B", "label": "player_B6", "name": "Leon Heinze", "leg": "left"},
+    "74": {"type": "player", "team": "B", "label": "player_B6", "name": "Leon Heinze", "leg": "right"},
+    "75": {"type": "player", "team": "B", "label": "player_B7", "name": "Leo Langhans", "leg": "left"},
+    "44": {"type": "player", "team": "B", "label": "player_B7", "name": "Leo Langhans", "leg": "right"}
 }
 
 #######################################
@@ -396,7 +396,7 @@ def main():
                 #
                 if players[this_player]["location"][1] > teams[this_team]["own-half-min"] and players[this_player]["location"][1] < teams[this_team]["own-half-max"]:
                     print_results_new(PREFIX_SO + this_player, RDF_TYPE, PREFIX_SO + "PlayerInOwnHalf", tmp_ts_str, tmp_t_str)
-                #else:
+                else:
                     print_results_new(PREFIX_SO + this_player, RDF_TYPE, PREFIX_SO + "PlayerNotInOwnHalf", tmp_ts_str, tmp_t_str)
                 ###############################################
 
