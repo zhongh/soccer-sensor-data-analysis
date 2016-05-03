59f297ee6e08125de90fd570b38a8d9b69936c00
zhonghao1014 <zhonghao1014@gmail.com>
2016-05-02 22:37:20 -0400

    2nd half data

--- output_time_intervals_2nd_half.py
+++ output_time_intervals_2nd_half.py
@@ -4,12 +4,12 @@ __author__ = 'Hao'
 OUTPUT_TIME_INTERVALS = [
     {"file-name": "sample-data-2nd-00m52s", "start": 52, "stop": 55.8, "note": "A, offside position, no ball touch, no consequence"},
     {"file-name": "sample-data-2nd-10m30s", "start": 629.8, "stop": 631.8, "note": "B, maybe not offside position, maybe ball touch, no consequence"},
-    {"file-name": "sample-data-2nd-10m45s", "start": 644.8, "stop": 646.5, "note": "A, offside position, ball touch, no consequence"},
-    {"file-name": "sample-data-2nd-12m16s", "start": 736, "stop": 738.5, "note": "B, maybe offside position, header, shot on goal"},
-    {"file-name": "sample-data-2nd-17m38s", "start": 1058, "stop": 1061, "note": "A, maybe in offside position, ball touch, shot on goal"},
-    {"file-name": "sample-data-2nd-20m25s", "start": 1225.3, "stop": 1228.7, "note": "B, maybe not in offside position, ball touch, no consequence"},
+    {"file-name": "sample-data-2nd-10m45s", "start": 645.2, "stop": 646.4, "note": "A, offside position, ball touch, no consequence"},
+    {"file-name": "sample-data-2nd-12m16s", "start": 735.5, "stop": 738.5, "note": "B, maybe offside position, header, shot on goal"},
+    {"file-name": "sample-data-2nd-17m38s", "start": 1058.7, "stop": 1061, "note": "A, maybe in offside position, ball touch, shot on goal"},
+    {"file-name": "sample-data-2nd-20m25s", "start": 1224.8, "stop": 1228.7, "note": "B, maybe not in offside position, ball touch, no consequence"},
     {"file-name": "sample-data-2nd-21m43s", "start": 1303.5, "stop": 1307, "note": "B, maybe offside position, shot on goal"},
     {"file-name": "sample-data-2nd-24m46s", "start": 1486, "stop": 1491.5, "note": "B, offside position, ball touch, no consequence"},
-    {"file-name": "sample-data-2nd-28m11s", "start": 1691, "stop": 1693, "note": "A, maybe offside position, ball touch, no consequence"},
+    {"file-name": "sample-data-2nd-28m11s", "start": 1691, "stop": 1693.6, "note": "A, maybe offside position, ball touch, no consequence"},
     {"file-name": "sample-data-2nd-28m24s", "start": 1704, "stop": 1709.5, "note": "A, maybe not offside position, ball touch, goal"}
 ]
